efee67030c4559424d0a93bd258bedf02dec0c4f
gildong-kwak <165612553+gildong-kwak@users.noreply.github.com>
2024-06-17 18:05:12 +0900

    240617

--- script/script.js
+++ script/script.js
@@ -1,34 +1,15 @@
 /* 변수 */
-const main = document.querySelector('main')
-const menu = document.querySelector('.left')
-const nav = document.querySelector('nav')
-const gnb = document.querySelectorAll('.gnb')
-const gnb_li = document.querySelectorAll('li')
-const lnb = document.querySelector('.lnb_fun')
+const menu = document.querySelectorAll('.gnb > li')
+const lnb = document.querySelector('.lnb_func')
 
-console.log(menu, nav, gnb, lnb)
+console.log(menu, lnb)
 
-/* 숨기기 */
-nav.style.display = 'none';
 lnb.style.display = 'none';
 
-/* nav 마우스  보이기 */
-menu.addEventListener('click',()=>{
-    nav.style.display = 'block';
-})
-/* nav 마우스 내리면 숨기기 */
-main.addEventListener('mouseover',()=>{
-    nav.style.display = 'none';
-})
-
-/* lnb 마우스 올리면 보이기 */
-gnb_li[1].addEventListener('mouseover',()=>{
+/* 효과 */
+menu[0].addEventListener('mouseover',function(){
     lnb.style.display = 'flex';
 })
-/* lnb 마우스 내리면 숨기기 */
-/* gnb_li[1].addEventListener('mouseout',()=>{
-    lnb.style.display = 'flex';
-}) */
-lnb.addEventListener('mouseout',()=>{
+menu[0].addEventListener('mouseout',function(){
     lnb.style.display = 'none';
 })
